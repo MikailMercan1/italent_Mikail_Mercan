7880f125f822687ba65e5459a6f459465984b847
MikailMercan1 <mikail.mercan@student.pxl.be>
2021-03-14 15:26:01 +0100

    implemented json data

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
+import { DataService } from '../data.service'; 
+import * as jsonData from '../../assets/data-events/events.json';
 
 @Component({
   selector: 'app-event',
@@ -9,13 +10,26 @@ import { DataService } from '../data.service';
 export class EventComponent implements OnInit {
 
   data: string;
+  events = ["event1", "event2", "event3"]
+  
+  products: any = (jsonData as any).default;
+  title: string;
+  text: string;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.data = this.dataService.data;
     this.dataService.data = undefined;
-    console.log(this.data)
+    const index = this.events.indexOf(this.data);
+    if(index > -1){
+      console.log()
+      this.title = this.products[index].name
+      this.text = this.products[index].text
+    } else {
+      this.title = "Something went wrong."
+    }
+    
   }
 
 }
