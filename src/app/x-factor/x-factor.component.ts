180ecfb150e63bb78735041af5f08dbdc9e76562
MikailMercan1 <mikail.mercan@student.pxl.be>
2021-03-20 17:19:29 +0100

    make x factor page

--- src/app/x-factor/x-factor.component.ts
+++ src/app/x-factor/x-factor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Draggable from "gsap/Draggable"; 
+import { TimelineMax, TweenLite, Linear, Power1 } from "gsap/all";  
 
 @Component({
   selector: 'app-x-factor',
@@ -10,6 +12,12 @@ export class XFactorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const draggable = Draggable.create("#outer-circle", {
+      type: "rotation",
+      inertia: true
+    });
+
   }
 
 }
