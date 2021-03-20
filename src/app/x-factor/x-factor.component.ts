4a180e2c82f03516ab2fafef146463cf65c0b9e1
MikailMercan1 <mikail.mercan@student.pxl.be>
2021-03-20 20:33:24 +0100

    made x factor page better

--- src/app/x-factor/x-factor.component.ts
+++ src/app/x-factor/x-factor.component.ts
@@ -15,9 +15,44 @@ export class XFactorComponent implements OnInit {
 
     const draggable = Draggable.create("#outer-circle", {
       type: "rotation",
-      inertia: true
+      inertia: true,
+      snap: function(endValue) { 
+        var position = endValue
+        var counter = 0
+        if(endValue >= 0){
+          while(position > 360){
+            position -= 360
+            counter += 1
+          }
+          if(position >= 0 && position < 90){
+            return 23 + (counter * 360)
+          } else if(position >= 90 && position < 180){
+            return 156 + (counter * 360)
+          } else if(position >= 180 && position < 270){
+            return 203 + (counter * 360)          
+          } else{
+            return 337 + (counter * 360)
+          } 
+        
+        } else {
+          while(position < -360){
+            position += 360
+            counter += 1
+          }
+          if(position <= 0 && position > -90){
+            return -23 - (counter * 360)
+          } else if(position <= -90 && position > -180){
+            return -156 - (counter * 360)
+          } else if(position <= -180 && position > -270){
+            return -203 - (counter * 360)          
+          } else{
+            return -337 - (counter * 360)
+          } 
+        }
+        
+      
+      }
     });
-
   }
 
 }
