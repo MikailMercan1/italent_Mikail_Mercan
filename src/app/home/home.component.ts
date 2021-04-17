3c38032c021b32a91dda0c59572fbf0a48aafb28
MikailMercan1 <mikail.mercan@student.pxl.be>
2021-04-17 17:03:09 +0200

    improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,9 @@ export class HomeComponent implements OnInit {
 
   products = (jsonData as any).default;
   titles = []
+  dates = []
+  companies = []
+  places = []
   descriptions = []
 
   constructor() { }
@@ -18,14 +21,24 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     for (let item of this.products){
       this.titles.push(item.name)
+      this.dates.push(item.date)
+      this.companies.push(item.company)
+      this.places.push(item.place)
       this.descriptions.push(item.text)
     }
     for (let i = 0; i <this.titles.length; i++){
       var idTitle = "title" + i
       var idDescription = "description" + i
       var idCard = "card" + i
+      var idDate = "date" + i
+      var idCompany = "company" + i
+      var idPlace = "place" + i
       document.getElementById(idTitle).innerHTML = this.titles[i];
-      document.getElementById(idDescription).innerHTML = this.descriptions[i];
+      document.getElementById(idDate).innerHTML = "<b>Datum: </b>" + this.dates[i];
+      document.getElementById(idCompany).innerHTML = "<b>Bedrijf: </b>" + this.companies[i];
+      document.getElementById(idPlace).innerHTML = "<b>Locatie: </b>" + this.places[i];
+      document.getElementById(idDescription).innerHTML = "<b>Beschrijving: </b>" + this.descriptions[i];
+      
       if(i % 2 == 0){
         document.getElementById(idCard).style.backgroundColor = "lightgrey"
       }
